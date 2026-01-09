3159944f037997d7ecb4e1b4226507550faefd2e
alitheowbot <83054010+alitheowbot@users.noreply.github.com>
2026-01-08 22:30:06 -0500

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,10 @@
+all: main.exe
+
+main.exe: main.o
+	g++ -o main.exe main.o
+
+main.o: main.cc
+	g++ -c main.cc
+
+run: main.exe
+	./main.exe
---
+++ main.cc
@@ -0,0 +1,100 @@
+// keep getting user input until a specific prompt
+// divide the users input by colons (:) to determine hh mm ss
+
+// our formula is totaltime / 0.6 * speed
+// e.g: to calculate 30 minutes of travel at 100kmph, you get 30 / 0.6 * 100 = 5000 (50km)
+// 
+
+// todo: 
+// fix colons (works as long as format is STRICTLY followed, no errors/exceptions are thrown) (WIEGAND WOULD BE DISAPPOINTED.)
+// output shows only "1" rather than "01"
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Log {
+    int hour_;
+    int min_;
+    int sec_;
+    float km_;
+};
+
+int main() {
+    Log storer;
+    vector<Log> distances = {};
+    string keepgo = "";
+    string q = "";
+    char dummy;
+    int kmph = 0; int newkmph;
+    float totalKM = 0.0; float newKM;
+    
+    int hour = 00; int tmphour = 00; int newhour;
+    int min = 00; int tmpmin = 00; int newmin;
+    int sec = 00; int tmpsec = 00; int newsec;
+
+    printf("Format: \nhour:minute:second KM\n");
+    
+    getline(cin,keepgo);
+    while (keepgo != "q") {
+
+        
+        if (keepgo == "q") break;
+
+
+        
+        stringstream ss(keepgo);
+        while (!ss.eof()) {
+            tmphour = hour;
+            tmpmin = min;
+            tmpsec = sec;
+            ss >> hour >> dummy >> min >> dummy >> sec >> newkmph;
+        }
+        if (kmph == 0) {
+            kmph = newkmph;
+            storer.hour_ = tmphour;
+            storer.min_ = tmpmin;
+            storer.sec_ = tmpsec;
+            storer.km_ = totalKM;
+            distances.push_back(storer);
+            getline(cin, keepgo);
+        }
+        else {
+            //cout << tmphour << "::" << tmpmin << "::" << tmpsec << " " << setprecision(2) << fixed << totalKM << " km" << endl;
+            
+            
+            // converting all to min
+            newhour = hour - tmphour;
+            newhour *= 60;
+
+            newsec = sec - tmpsec;
+            newsec *= 0.6;
+
+            newmin = (min - tmpmin) + newhour + newsec;
+
+            newKM = (newmin / 0.6 * kmph) / 100;
+            totalKM += newKM;
+
+            storer.hour_ = hour;
+            storer.min_ = min;
+            storer.sec_ = sec;
+            storer.km_ = totalKM;
+            distances.push_back(storer);
+
+            //cout << hour << "::" << min << "::" << sec << " " << setprecision(2) << fixed << totalKM << " km" << endl;
+            kmph = newkmph;
+
+            getline(cin, keepgo);
+        }
+    }
+    for (Log x: distances) {
+        cout << x.hour_ << "::" << x.min_ << "::" << x.sec_ << " " << x.km_ << " km" << endl;
+    }
+
+    
+    
+}
